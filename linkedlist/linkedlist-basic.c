9baf15b6d9e93851183c594a081067ecf06c6a33
emreozgenc <emreozgenc@hotmail.com.tr>
2020-07-04 14:22:07 +0300

    added linked list structure and add function

---
+++ linkedlist/linkedlist-basic.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/* ## Linked list structure ## */
+typedef struct n {
+	struct n * next;
+	int x;
+}Node;
+
+/* ## Add function ## */
+Node * addNode(Node * r, int x) {
+	if(r == NULL) {
+		r = (Node *)malloc(sizeof(Node));
+		r->x = x;
+		r->next = NULL;
+		return r;
+	}
+	
+	Node * iter = r; // Iterator for not lose root
+	while(iter->next != NULL) {
+		iter = iter->next;
+	}
+	
+	Node * newNode = (Node *)malloc(sizeof(Node));
+	newNode->x = x;
+	newNode->next = NULL;
+	iter->next = newNode;
+	return r;	
+}
+
+int main() {
+	Node * root = NULL;
+	
+}
